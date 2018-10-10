ab23776efeb2dcea2033d31f877a1794d2d553e6
Obed D <obed@obeds-macbook-pro.local>
2018-10-10 10:36:57 +0700

    Initial Commit

---
+++ Aplikasi Perhitungan/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Aplikasi Perhitungan
+//
+//  Created by Obed RPL D on 09/10/18.
+//  Copyright © 2018 Syams.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var txtHpp: UITextField!
+    @IBOutlet weak var txtLpp: UITextField!
+    @IBOutlet weak var txtPpp: UITextField!
+    @IBOutlet weak var txtsisi1: UITextField!
+    @IBOutlet weak var txtsisi2: UITextField!
+    @IBOutlet weak var txtHasil: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnProses(_ sender: Any) {
+       let lebarP = Double(txtsisi1.text!)
+       let LebarP2 = Double(txtsisi2.text!)
+        let Jumlah = lebarP! * LebarP2!
+        txtHasil.text = "\(Jumlah)"
+    }
+    @IBAction func btnpp(_ sender: Any) {
+        let lebarPPP = Double(txtPpp.text!)
+        let lebarLPP = Double(txtLpp.text!)
+        let Hasil = lebarPPP! * lebarLPP!
+        txtHpp.text = "\(Hasil)"
+        
+    }
+}
+
